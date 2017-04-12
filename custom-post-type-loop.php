075f0b600e84f277227c76e71e963e72fcf0a616
amitbaral <supper_a@hotmail.com>
2017-04-12 20:59:57 +1000

    Update custom-post-type-loop.php

--- custom-post-type-loop.php
+++ custom-post-type-loop.php
@@ -1,7 +1,7 @@
-<!--- *************************************
+<!-- *************************************
 Simple custom post type loop
 In this case my custom post type is slider  
-************************************* --->
+************************************* -->
 
 <?php $loop = new WP_Query( array( 'post_type' => 'slider', 'posts_per_page' => -1 ) ); ?>
   <?php while ( $loop->have_posts() ) : $loop->the_post(); ?>
